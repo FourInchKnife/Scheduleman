1f28c9ca9ecde19b8c10a5580c41e49b46a0d24a
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-05-22 18:46:03 -0400

    Update task.py

--- task.py
+++ task.py
@@ -5,7 +5,7 @@ import shlex
 indicators={'a': '\U0001f1e6', 'b': '\U0001f1e7', 'c': '\U0001f1e8', 'd': '\U0001f1e9', 'e': '\U0001f1ea', 'f': '\U0001f1eb', 'g': '\U0001f1ec', 'h': '\U0001f1ed', 'i': '\U0001f1ee', 'j': '\U0001f1ef', 'k': '\U0001f1f0', 'l': '\U0001f1f1', 'm': '\U0001f1f2', 'n': '\U0001f1f3', 'o': '\U0001f1f4', 'p': '\U0001f1f5', 'q': '\U0001f1f6', 'r': '\U0001f1f7', 's': '\U0001f1f8', 't': '\U0001f1f9', 'u': '\U0001f1fa', 'v': '\U0001f1fb', 'w': '\U0001f1fc', 'x': '\U0001f1fd', 'y': '\U0001f1fe', 'z': '\U0001f1ff'}
 days='mtwhfs'
 
-client = discord.Client(allowed_mentions=discord.AllowedMentions())
+client = discord.Client()
 
 @client.event
 async def on_ready():
