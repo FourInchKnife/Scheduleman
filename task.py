4496a0d821eae3a99202e3974b3df8350071e73b
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-05-23 10:23:15 -0400

    Update task.py

--- task.py
+++ task.py
@@ -3,7 +3,11 @@ import discord
 import shlex
 
 indicators={'a': '\U0001f1e6', 'b': '\U0001f1e7', 'c': '\U0001f1e8', 'd': '\U0001f1e9', 'e': '\U0001f1ea', 'f': '\U0001f1eb', 'g': '\U0001f1ec', 'h': '\U0001f1ed', 'i': '\U0001f1ee', 'j': '\U0001f1ef', 'k': '\U0001f1f0', 'l': '\U0001f1f1', 'm': '\U0001f1f2', 'n': '\U0001f1f3', 'o': '\U0001f1f4', 'p': '\U0001f1f5', 'q': '\U0001f1f6', 'r': '\U0001f1f7', 's': '\U0001f1f8', 't': '\U0001f1f9', 'u': '\U0001f1fa', 'v': '\U0001f1fb', 'w': '\U0001f1fc', 'x': '\U0001f1fd', 'y': '\U0001f1fe', 'z': '\U0001f1ff'}
-days='mtwhfs'
+
+def makeInd(letter): ## Python magic does the same as the original thing
+    lets="abcdefghijklmnopqrstuvwxyz"
+    maked=eval('"\\U000'+hex(lets.index(letter)+127462)[2:]+'"')
+    return maked
 
 client = discord.Client()
 
@@ -42,8 +46,8 @@ async def on_message(message):
         else:
             await message.channel.send("I'm not sure what ```{0}``` is supposed to be \U0001F610".format())
     elif message.author==message.channel.guild.me and message.content.startswith("post:"):
-        for i in days:
-            await message.add_reaction(indicators[i])
+        for i in 'mtwhf:
+            await message.add_reaction(makeInd(i))
         await message.add_reaction('\U0000274C')
         await message.edit(content=message.content.split("post: ",1)[1])
     elif message.author==message.channel.guild.me and message.content.startswith("poll: "):
