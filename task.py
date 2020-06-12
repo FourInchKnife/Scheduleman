63fd811b0dc723935431ed294cfd7536506d3e59
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-06-12 15:30:46 -0400

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,7 @@
 from os import environ
 import discord
 import shlex
+from discord import AllowedMentions
 
 indicators={'a': '\U0001f1e6', 'b': '\U0001f1e7', 'c': '\U0001f1e8', 'd': '\U0001f1e9', 'e': '\U0001f1ea', 'f': '\U0001f1eb', 'g': '\U0001f1ec', 'h': '\U0001f1ed', 'i': '\U0001f1ee', 'j': '\U0001f1ef', 'k': '\U0001f1f0', 'l': '\U0001f1f1', 'm': '\U0001f1f2', 'n': '\U0001f1f3', 'o': '\U0001f1f4', 'p': '\U0001f1f5', 'q': '\U0001f1f6', 'r': '\U0001f1f7', 's': '\U0001f1f8', 't': '\U0001f1f9', 'u': '\U0001f1fa', 'v': '\U0001f1fb', 'w': '\U0001f1fc', 'x': '\U0001f1fd', 'y': '\U0001f1fe', 'z': '\U0001f1ff'}
 
@@ -21,7 +22,7 @@ async def on_message(message):
         params=cmd.split(" ",1)[:]
         if params[0] in ['post','poll']:
             if message.author!=client.user:
-                await message.channel.send('{0}: {1} asked: {2}'.format(params[0],message.author.display_name,params[1]),allowed_mentions=discord.AllowedMentions(everyone=message.author.permissions_in(message.channel).mention_everyone))
+                await message.channel.send('{0}: {1} asked: {2}'.format(params[0],message.author.display_name,params[1]),allowed_mentions=AllowedMentions(everyone=message.author.permissions_in(message.channel).mention_everyone))
             await message.delete(delay=2)
     elif message.author==message.channel.guild.me and message.content.startswith("post:"):
         for i in 'mtwhfs':
