274596be4dd16cf30d17801b5b57f216393133c8
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-06-12 19:46:47 -0400

    Update task.py

--- task.py
+++ task.py
@@ -11,26 +11,30 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    await client.change_presence(activity=discord.Game(name="!post and !poll"))
+    await client.change_presence(activity=discord.Game(name="!day !week and !poll"))
 @client.event
 async def on_message(message):
     if message.content.startswith('!'):
         cmd=message.content[1:]
         params=cmd.split(" ",1)[:]
-        if params[0] in ['post','poll']:
+        if params[0] in ['week','poll','day']:
             if message.author!=client.user:
                 pingable=discord.AllowedMentions(everyone=message.author.permissions_in(message.channel).mention_everyone)
                 await message.channel.send('{0}: {1} asked: {2}'.format(params[0],message.author.display_name,params[1]),allowed_mentions=pingable)
             await message.delete(delay=2)
-    elif message.author==message.channel.guild.me and message.content.startswith("post:"):
+    elif message.author==message.channel.guild.me and message.content.startswith("week:"):
         for i in 'mtwhfs':
             await message.add_reaction(makeInd(i))
         await message.add_reaction('\U0000274C')
-        await message.edit(content=message.content.split("post: ",1)[1])
+        await message.edit(content=message.content.split("week: ",1)[1])
     elif message.author==message.channel.guild.me and message.content.startswith("poll: "):
         await message.add_reaction('\U00002705')
         await message.add_reaction('\U0000274C')
         await message.edit(content=message.content.split("poll: ",1)[1])
+    elif message.author==message.channel.guild.me and message.content.startswith("day: "):
+        await message.channel.send('This command has yet to be fully implemented \U00002639')
+        await message.edit(content=message.content.split("day: ",1)[1])
+        
 
 bot_token=environ.get('BOT_TOKEN',None)
 if not bot_token:
