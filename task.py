62b6426b88bd53d0b01e2c0726f7e03df282af74
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-06-12 15:15:29 -0400

    Update task.py

--- task.py
+++ task.py
@@ -18,7 +18,8 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('!'):
         cmd=message.content[1:]
-        params=cmd.split(" ",1)
+        params=cmd.split(" ",1)[:]
+        print(params)
         if params[0] in ['post','poll']:
             if message.author!=client.user:
                 await message.channel.send('{0}: {2} {1} asked: {3}'.format(params[0],message.author.display_name,params[1]),allowed_mentions=discord.AllowedMentions(everyone=message.author.permissions_in(message.channel).mention_everyone))
