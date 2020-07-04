6135395c2dd1bc0ae91146075176fa73eec6ebdc
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-07-04 15:02:13 -0400

    Update task.py

--- task.py
+++ task.py
@@ -4,49 +4,44 @@ from discord import AllowedMentions,Game
 import datetime
 
 bot = commands.Bot(command_prefix='!',owner_id=600130839870963725,activity=Game(name="!days and !poll",start=datetime.datetime.now()))
+
+emoji_list=[]
+emojiGuild=bot.get_guild(712731280772694198).emojis
+emojiIDs=[721544022534914130,721544022216278056,721544022534914120,721544022547759165,721544022237249538,721544022081929226,721544022384181339]
+for i in emojiGuild:
+    for j in emojiIDs:
+        if i.id==j:
+            emoji_list.append(i)
+
 @bot.event
 async def on_ready():
     print('Logged in as {}'.format(bot.user))
 @bot.command()
 async def poll(ctx,*,arg):
-    message=ctx.send("{0} asked: {1}".format(ctx.author.mention,arg),allowed_mentions=AllowedMentions(
-    
-    if message.content.startswith('!'):
-        cmd=message.content[1:]
-        params=cmd.split(" ",1)[:]
-        if params[0] in ['days','poll','time']:
-            if message.author!=client.user:
-                if type(message.channel.guild.me)==type(message.author):
-                    pingable=discord.AllowedMentions(everyone=message.author.permissions_in(message.channel).mention_everyone)
-                else:
-                    pingable=discord.AllowedMentions(everyone=True)
-                await message.channel.send('{0}: {1} asked: {2}'.format(params[0],message.author.display_name,params[1]),allowed_mentions=pingable)
-            await message.delete(delay=2)
-        elif params[0] == 'clocktest':
-            toSend=''
-            for i in ['1','2','3','4','5','6','7','8','9','10','11','12']:
-                toSend+=':clock{}: :clock{}30: '.format(i,i)
-            await message.channel.send(toSend+'<:d20:712019129237897329>')
-    elif message.author==message.channel.guild.me and message.content.startswith("days:"):
-        emojiGuild=client.get_guild(712731280772694198)
-        emojiIDs=[721544022534914130,721544022216278056,721544022534914120,721544022547759165,721544022237249538,721544022081929226,721544022384181339]
-        for i in emojiIDs:
-            nextEmoji= await emojiGuild.fetch_emoji(i)
-            await message.add_reaction(nextEmoji)
-        await message.add_reaction('\U0000274C')
-        await message.edit(content=('filler'+message.content).split("days: ",1)[1],allowed_mentions=discord.AllowedMentions(everyone=message.mention_everyone))
-    elif message.author==message.channel.guild.me and message.content.startswith("poll: "):
-        await message.add_reaction('\U00002705')
-        await message.add_reaction('\U0000274C')
-        await message.edit(content=('filler'+message.content).split("poll: ",1)[1],allowed_mentions=discord.AllowedMentions(everyone=message.mention_everyone))
-    elif message.author==message.channel.guild.me and message.content.startswith("time: "):
-        await message.channel.send('This command has yet to be fully implemented \U00002639')
-        await message.edit(content=('filler'+message.content).split("time: ",1)[1],allowed_mentions=discord.AllowedMentions(everyone=message.mention_everyone))
-    elif message.author.id== 270904126974590976:
-        await message.add_reaction('❤️')
+    allButAuthor=[]
+    for i in ctx.guild.members:
+        if i != ctx.author:
+            allButAuthor.append(i)
+    pingable=AllowedMentions(everyone=ctx.author.permissions_in(ctx.channel).mention_everyone,roles=ctx.author.permissions_in(ctx.channel).mention_everyone,users=allButAuthor)
+    sent_message=ctx.send("{0} asked: {1}".format(ctx.author.mention,arg),allowed_mentions=pingable)
+    await sent_message.add_reaction('\U00002705')
+    await sent_message.add_reaction('\U0000274C')
+@bot.command()
+async def days(ctx,*,arg):
+    allButAuthor=[]
+    for i in ctx.guild.members:
+        if i != ctx.author:
+            allButAuthor.append(i)
+    pingable=AllowedMentions(everyone=ctx.author.permissions_in(ctx.channel).mention_everyone,roles=ctx.author.permissions_in(ctx.channel).mention_everyone,users=allButAuthor)
+    sent_message=ctx.send("{0} asked: {1}".format(ctx.author.mention,arg),allowed_mentions=pingable)
+    for i in emoji_list:
+        await sent_message.add_reaction(i)
+@bot.event #super simple error handling that sends the exception in a discord message
+async def on_command_error(context,exception):
+    await context.send("Error: ```{}```".format(str(exception)))
 
 bot_token=environ.get('BOT_TOKEN',None)
 if not bot_token:
     bot_token=input('What is your bot token?')
     print('\n'*100)
-client.run(bot_token)
+bot.run(bot_token)
