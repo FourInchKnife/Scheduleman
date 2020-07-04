a8db8faec444dffb9c2aa75ba9f052d381eec956
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-07-04 15:46:41 -0400

    Update task.py

--- task.py
+++ task.py
@@ -5,11 +5,6 @@ import datetime
 
 bot = commands.Bot(command_prefix='!',owner_id=600130839870963725,activity=Game(name="!days and !poll",start=datetime.datetime.now()))
 
-emoji_list=[]
-emojiIDs=[721544022534914130,721544022216278056,721544022534914120,721544022547759165,721544022237249538,721544022081929226,721544022384181339]
-for i in emojiIDs:
-    emoji_list.append(bot.get_emoji(i))
-
 @bot.event
 async def on_ready():
     print('Logged in as {}'.format(bot.user))
