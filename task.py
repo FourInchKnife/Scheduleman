102d31c5bbb66a740fd7619fd4e1a1023cb588dc
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-07-04 15:13:33 -0400

    Update task.py

--- task.py
+++ task.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix='!',owner_id=600130839870963725,activity=Game(
 emoji_list=[]
 emojiIDs=[721544022534914130,721544022216278056,721544022534914120,721544022547759165,721544022237249538,721544022081929226,721544022384181339]
 for i in emojiIDs:
-    emoji_list.append(get_emoji(i))
+    emoji_list.append(bot.get_emoji(i))
 
 @bot.event
 async def on_ready():
